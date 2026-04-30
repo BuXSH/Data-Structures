30ff2a2e8d10ae6f2107fbe6946eab454824171a
BZZYL <bzzyl@foxmail.com>
2026-04-30 21:48:47 +0800

    13

--- 线性表/顺序表/顺序表(静态分配实现).c
+++ 线性表/顺序表/顺序表(静态分配实现).c
@@ -19,7 +19,7 @@ void InitList(SqList *L){
     L->length=0;
 }
 
-// 顺序表的插入(时间复杂度：O(n))
+// 顺序表的插入(平均时间复杂度：O(n))
 // 注意： i是位序，是从1开始的，数组实际下标是从开始
 // 参数：(顺序表，位序，值)
 int ListInsert(SqList *L,int i,ElemType e){
@@ -34,7 +34,7 @@ int ListInsert(SqList *L,int i,ElemType e){
     }
     // 2.将第i个位置之后的元素往后移动
     // 从最后一位元素往后移动
-    for (int j = L->length-1; j > i-1; --j) {
+    for (int j = L->length-1; j >= i-1; --j) {
         L->data[j+1]=L->data[j];
     }
     // 3.插入操作
@@ -47,7 +47,7 @@ int ListInsert(SqList *L,int i,ElemType e){
     return 1;
 }
 
-// 顺序表的删除(时间复杂度：O(n))
+// 顺序表的删除(平均时间复杂度：O(n))
 // 注意： i是位序，是从1开始的，数组实际下标是从开始
 // 参数：(顺序表，位序，接收被删除的元素的值(需要*))
 int DeleteList(SqList *L,int i,ElemType *d){
@@ -64,12 +64,32 @@ int DeleteList(SqList *L,int i,ElemType *d){
     // 3.删除操作
     // 把被删除元素后的元素往前移
     for (int j = i; j < L->length; ++j) {
-        L->data[i-1]=L->data[i];
+        L->data[j-1]=L->data[j];
     }
     // 表长减1
     L->length--;
 
     return 1;
+}
+
+// 按值查找(平均时间复杂度：O(n))
+// 参数：(顺序表，查找的值)
+// 返回值：位序
+int LocateElem(SqList L,int e){
+    for (int i = 0; i < L.length; ++i) {
+        if(L.data[i]==e){
+            return i+1;
+        }
+    }
+    return 0;
+}
+
+// 按位查找(时间复杂度：O(1))
+// “随机存取”的特性
+// 参数：(顺序表，位序)
+// 返回值：值
+ElemType GetElem(SqList L,int i){
+    return L.data[i-1];
 }
 
 // 输出
@@ -82,19 +102,28 @@ void PrintList(SqList L){
 int main() {
     SqList L;
     InitList(&L);
+    L.data[0]=1;
+    L.data[1]=12;
+    L.data[2]=23;
+    L.data[3]=34;
+    L.data[4]=45;
+    L.data[5]=56;
+    L.data[6]=67;
+    L.length=7;
 
-    int e=1;
+    int e=9;
     int result=ListInsert(&L,1,e);
     printf("result=%d\n",result);
-    e=6;
-    result=ListInsert(&L,2,e);
-    printf("result=%d\n",result);
+
     PrintList(L);
 
     int d;
-    DeleteList(&L,2,&d);
+    DeleteList(&L,3,&d);
     printf("Delete=%d\n",d);
-    printf("result=%d\n",result);
+
     PrintList(L);
+
+    printf("56 LocateElem:%d\n",LocateElem(L,56));
+    printf("4 GetElem:%d\n",GetElem(L,4));
     return 0;
 }
